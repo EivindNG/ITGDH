ac53a54b3ba7ef2aec26c8e91c0d839522160d84
EivindNG <eivindng@stud.ntnu.no>
2019-03-13 15:04:54 +0100

    Commit Message

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,25 +1,38 @@
+
+import tgdh.TgdhException;
+import tgdh.TgdhGroupIdentifier;
 import tgdh.TreeGroupDiffieHellman;
+import tgdh.crypto.TgdhKeySpec;
 import tgdh.tree.*;
 
 public class Main {
-    public static void main(String[] args){
-
+    public static void main(String[] args) throws TgdhException {
+        TgdhKeySpec specs = new TgdhKeySpec();
         LeafNode M1 = new LeafNode("M1");
         LeafNode M2 = new LeafNode("M2");
         LeafNode M3 = new LeafNode("M3");
-        LeafNode M4 = new LeafNode("M4");
+
+        TreeGroupDiffieHellman test = new TreeGroupDiffieHellman();
+        test.newGroup();
 
         Node[] nodes = new Node[]{
-                new Node(), new Node(), M1, M2, new Node(), M3, M4};
+                new Node(), new Node(), M1, M2, M3};
 
         TreeInfo tinfo = new TreeInfo(
                 nodes, TreeInfo.PREORDER,
                 "M1");
+
         // build the tree
         BasicTree atree = new BasicTree(tinfo);
         Tree tre = new Tree(tinfo);
         //expected tree
-        String expectedTree = "Preorder: <0,0>, <1,0>, <2,0>(M1), <2,1>(M2), <1,1>, <2,2>(M3), <2,3>(M4)";
-        System.out.println(tre.height());
+        System.out.println("tre");
+        System.out.println(tre);
+
+        TreeInfo sponsor = (TreeInfo) tre.join(new LeafNode("M4"));
+
+        System.out.println(tre);
+
+        System.out.println();
     }
 }
